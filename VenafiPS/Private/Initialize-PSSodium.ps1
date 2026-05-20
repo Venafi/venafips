e037df9d129b7fc05c686bd7a08680d1011720be
Greg Brownstein <greg@jagtechnical.com>
2026-05-20 11:02:14 -0400

    fixed pssodium version

--- VenafiPS/Private/Initialize-PSSodium.ps1
+++ VenafiPS/Private/Initialize-PSSodium.ps1
@@ -11,11 +11,11 @@ function Initialize-PSSodium {
     }
 
     # Check if the module is installed
-    $module = Get-Module PSSodium -ListAvailable | Where-Object { $_.Version -eq '0.4.2' }
+    $module = Get-Module PSSodium -ListAvailable | Where-Object { $_.Version -eq $script:pssodiumVersion }
 
     if ( -not $module ) {
         if ( $Force ) {
-            Install-Module -Name PSSodium -Repository PSGallery -Force -RequiredVersion '0.4.2'
+            Install-Module -Name PSSodium -Repository PSGallery -Force -RequiredVersion $script:pssodiumVersion
 
             # validate hash
             $modulePath = $module.ModuleBase
@@ -32,12 +32,12 @@ function Initialize-PSSodium {
             }
         }
         else {
-            throw 'The PSSodium module is not installed.  Add -Force for the module to be automatically installed or install v0.4.2 from the PowerShell Gallery.'
+            throw "The PSSodium module is not installed.  Add -Force for the module to be automatically installed or install v$script:pssodiumVersion from the PowerShell Gallery."
         }
     }
 
     try {
-        Import-Module PSSodium -Force -ErrorAction Stop
+        Import-Module -Name PSSodium -RequiredVersion $script:pssodiumVersion -Force -ErrorAction Stop
     }
     catch {
         throw "Sodium encryption could not be loaded.  Ensure you are running PowerShell v7+ and if on Windows, install the latest Visual C++ Runtime.  $_"
