529f5fcda5daefd8f867d081432f129294c18d62
Greg Brownstein <greg@jagtechnical.com>
2026-05-21 15:19:25 -0400

    Security hardening (#396)

--- VenafiPS/Private/Initialize-PSSodium.ps1
+++ VenafiPS/Private/Initialize-PSSodium.ps1
@@ -11,17 +11,33 @@ function Initialize-PSSodium {
     }
 
     # Check if the module is installed
-    if ( -not (Get-Module PSSodium -ListAvailable) ) {
+    $module = Get-Module PSSodium -ListAvailable | Where-Object { $_.Version -eq $script:pssodiumVersion }
+
+    if ( -not $module ) {
         if ( $Force ) {
-            Install-Module -Name PSSodium -Force -RequiredVersion '0.4.2'
+            $module = Install-Module -Name PSSodium -Repository PSGallery -Force -RequiredVersion $script:pssodiumVersion -PassThru
+
+            # validate hash
+            $modulePath = $module.InstalledLocation
+
+            $script:pssodiumHash | ForEach-Object {
+                $fullPath = Join-Path $modulePath $_.Path
+                $CurrentHash = (Get-FileHash $fullPath -Algorithm SHA256).Hash
+                if ($CurrentHash -ne $_.Hash) {
+                    throw "PSSodium file tampered with: $fullPath"
+                }
+                else {
+                    Write-Verbose "PSSodium file validated: $fullPath"
+                }
+            }
         }
         else {
-            throw 'The PSSodium module is not installed.  Add -Force for the module to be automatically installed or install from the PowerShell Gallery.'
+            throw "The PSSodium module is not installed.  Add -Force for the module to be automatically installed or install v$script:pssodiumVersion from the PowerShell Gallery."
         }
     }
 
     try {
-        Import-Module PSSodium -Force -ErrorAction Stop
+        Import-Module -Name PSSodium -RequiredVersion $script:pssodiumVersion -Force -ErrorAction Stop
     }
     catch {
         throw "Sodium encryption could not be loaded.  Ensure you are running PowerShell v7+ and if on Windows, install the latest Visual C++ Runtime.  $_"
